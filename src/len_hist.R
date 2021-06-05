26f9485d164dd540eab2f0a0d2d98e5e09f7bde6
Sergey Petrovich <serg-petrovich2000@yandex.ru>
2021-06-05 15:09:23 +0300

    Add length historgams

---
+++ src/len_hist.R
@@ -0,0 +1,14 @@
+source('lib.R')
+
+NAME <- 'H3K4me3_CH12.ENCFF150KSB.mm10'
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
---
+++ src/lib.R
@@ -0,0 +1,9 @@
+setwd("/home/sergey/Desktop/hse21_bioproject")
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+DATA_DIR <- './data/'
+OUT_DIR <- './results/'
