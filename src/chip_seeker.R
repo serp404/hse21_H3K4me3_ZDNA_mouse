c2bac88ce2f61296c963c175f6976920fdb2c03e
Sergey Petrovich <serg-petrovich2000@yandex.ru>
2021-06-06 14:06:45 +0300

    Add chip_seeker results

---
+++ src/chip_seeker.R
@@ -0,0 +1,23 @@
+source('lib.R')
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+# BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+
+library(ChIPseeker)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+
+###
+
+NAME <- 'H3K4me3_CH12.ENCFF150KSB.mm10.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.annopie.pdf'))
+plotAnnoPie(peakAnno)
+dev.off()
