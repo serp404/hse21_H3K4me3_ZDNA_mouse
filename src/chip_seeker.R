ba7ee0e6e033a82d116d15ac7830f0864c19d0cb
Sergey Petrovich <serg-petrovich2000@yandex.ru>
2021-06-08 22:11:46 +0300

    Fix chipSeeker db

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -10,13 +10,13 @@ library(clusterProfiler)
 
 ###
 
-NAME <- 'H3K4me3_CH12.ENCFF150KSB.mm10.filtered'
+NAME <- 'mouseZ-DNA1'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 ###
 
 txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
-peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Mm.eg.db")
 
 pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.annopie.pdf'))
 plotAnnoPie(peakAnno)
